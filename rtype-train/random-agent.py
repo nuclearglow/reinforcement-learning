2a519fcbd0736bf12a82550cb80b3fda770cdae8
fischmechanik <fischmechanik@gmail.com>
2022-04-26 23:44:12 +0200

    custom integration rtype

--- rtype-train/random-agent.py
+++ rtype-train/random-agent.py
@@ -1,8 +1,28 @@
+
+
 import retro
+import os
+
+# Get path of script
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 def main():
-    env = retro.make(game='SuperRType-Snes')
+
+    # Add custom integration
+    retro.data.Integrations.add_custom_path(
+                os.path.join(SCRIPT_DIR, "scenarios")
+        )
+
+    # Create gym environment with retro using custom integration
+    print("scenario1" in retro.data.list_games(inttype=retro.data.Integrations.ALL))
+    env = retro.make("scenario1", inttype=retro.data.Integrations.ALL)
+    print(env)
+
+    # Reset environment
     obs = env.reset()
+
+    # Loop...
     while True:
         obs, rew, done, info = env.step(env.action_space.sample())
         env.render()
