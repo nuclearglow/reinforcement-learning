be604fa71eedb3bed5b6306546fa5d8a1299650f
fischmechanik <fischmechanik@gmail.com>
2022-04-27 00:17:01 +0200

    tests with obs

--- rtype-train/random-agent.py
+++ rtype-train/random-agent.py
@@ -2,34 +2,54 @@
 
 import retro
 import os
+import matplotlib.pyplot as plt
 
 # Get path of script
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def main():
+#def main():
 
-    # Add custom integration
-    retro.data.Integrations.add_custom_path(
-                os.path.join(SCRIPT_DIR, "scenarios")
-        )
+# Add custom integration
+retro.data.Integrations.add_custom_path(
+            os.path.join(SCRIPT_DIR, "scenarios")
+    )
 
-    # Create gym environment with retro using custom integration
-    print("scenario1" in retro.data.list_games(inttype=retro.data.Integrations.ALL))
-    env = retro.make("scenario1", inttype=retro.data.Integrations.ALL)
-    print(env)
+# Create gym environment with retro using custom integration
+print("scenario1" in retro.data.list_games(inttype=retro.data.Integrations.ALL))
+env = retro.make("scenario1", inttype=retro.data.Integrations.ALL)
+print(env)
 
-    # Reset environment
-    obs = env.reset()
-
-    # Loop...
-    while True:
-        obs, rew, done, info = env.step(env.action_space.sample())
-        env.render()
-        if done:
-            obs = env.reset()
-    env.close()
+# Reset environment
+obs = env.reset()
 
 
-if __name__ == "__main__":
-    main()
+rews = []
+# Loop...
+for t in range(1000):
+    obs, rew, done, info = env.step(env.action_space.sample())
+    rews.append(rew)
+    #env.render()
+    if done:
+        obs = env.reset()
+env.close()
+
+print(rews)
+
+# Plot obs
+plt.imshow(obs[:,:,0], cmap='gray')
+plt.imshow(obs[:,:,1], cmap='gray')
+plt.imshow(obs[:,:,2], cmap='gray')
+
+
+# Loop...
+# while True:
+#     obs, rew, done, info = env.step(env.action_space.sample())
+#     env.render()
+#     if done:
+#         obs = env.reset()
+# env.close()
+
+
+#if __name__ == "__main__":
+#    main()
